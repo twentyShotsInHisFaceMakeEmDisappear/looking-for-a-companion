199dd332d60f3f5aefe43c36e7eb47d7a36d46a0
MDSI2508 <73603068+MDSI2508@users.noreply.github.com>
2023-03-13 22:02:03 +0300

    Navbar logic

--- src/Client/looking-for/src/Components/Navbar/Navbar.jsx
+++ src/Client/looking-for/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Navbar.css';
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+import Home from "../../Pages/Home/Home";
 
+const { Header, Sider, Content } = Layout;
 const Navbar = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
     return (
-        <>
-            <h2>navbar</h2>
-        </>
+        <Layout>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <UserOutlined />,
+                            label: 'nav 1',
+                        },
+                        {
+                            key: '2',
+                            icon: <VideoCameraOutlined />,
+                            label: 'nav 2',
+                        },
+                        {
+                            key: '3',
+                            icon: <UploadOutlined />,
+                            label: 'nav 3',
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                >
+                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                        className: 'trigger',
+                        onClick: () => setCollapsed(!collapsed),
+                    })}
+                </Header>
+                {/*<Content*/}
+                {/*    style={{*/}
+                {/*        margin: '24px 16px',*/}
+                {/*        padding: 24,*/}
+                {/*        minHeight: 280,*/}
+                {/*        background: colorBgContainer,*/}
+                {/*    }}*/}
+                {/*>*/}
+                {/*    <Home />*/}
+                {/*</Content>*/}
+            </Layout>
+        </Layout>
     );
-}
-export default Navbar();
+};
+export default Navbar;
